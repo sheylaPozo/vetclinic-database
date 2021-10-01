5682d261c5d562065b0ca28d9ade8bbd32f5ed79
sheylaPozo <shiggipoke@gmail.com>
2021-10-01 13:50:34 -0500

    create and modify tables, add queries

--- schema.sql
+++ schema.sql
@@ -9,3 +9,25 @@ CREATE TABLE animals (id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(100), da
 -- TASK 2 - Add a column species of type string to your animals --
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(100);
+
+-- TASK 3 - Query multiple tables  --
+
+CREATE TABLE owners  (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(100),
+  age INT,
+  PRIMARY KEY(id)
+)
+
+
+CREATE TABLE species  (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100),
+  PRIMARY KEY(id)
+)
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
