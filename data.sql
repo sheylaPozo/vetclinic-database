8f3adfb784b8d7ae4adc4cf3af9e6c07ae64b420
sheylaPozo <shiggipoke@gmail.com>
2021-10-01 13:51:35 -0500

    transaction to update tables, add more queries

--- data.sql
+++ data.sql
@@ -13,3 +13,30 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts )
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts ) VALUES ('Angemon', 'Jun 12, 2005', -45, true, 1 );
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts ) VALUES ('Boarmon', 'Jun 7, 2005', 20.4, true, 7 );
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts ) VALUES ('Blossom', 'Oct 13, 1998', 17, true, 3 );
+
+-- TASK 3 - Insert data
+
+INSERT INTO owners ( full_name, age ) VALUES ('Sam Smith', 34);
+INSERT INTO owners ( full_name, age ) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners ( full_name, age ) VALUES ('Bob', 45);
+INSERT INTO owners ( full_name, age ) VALUES ('Melody Pond', 77);
+INSERT INTO owners ( full_name, age ) VALUES ('Dean Winchester', 14);
+INSERT INTO owners ( full_name, age ) VALUES ('Jodie Whittaker', 38);
+
+
+INSERT INTO species ( name ) VALUES ('Pokemon');
+INSERT INTO species ( name ) VALUES ('Digimon');
+
+BEGIN TRANSACTION;
+UPDATE animals SET species_id = 1 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 4 WHERE species_id IS NULL;
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+UPDATE animals SET owner_id = 1 WHERE name LIKE 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+COMMIT TRANSACTION;
+
